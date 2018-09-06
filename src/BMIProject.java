9ef0d52de1f102d45fd28f71917fe991f6d335d1
Unknown <brandon.woolard.2019@student.lhschools.org>
2018-09-06 16:26:20 -0400

    Completed

--- src/BMIProject.java
+++ src/BMIProject.java
@@ -4,32 +4,29 @@ public class BMIProject {
 
     public static void main(String[] args) {
         String personsName;
-        /*String personsHeight;
-        String personsWeight;
-        String personsBMI;*/
-       
-        int personsHeight,personsWeight,heightModifier,weightModifier,personsBMI,
-        convertedHeight,convertedWeight;
-        
-        keyboard = new Scanner(System.in);
-        heightModifier = 0.0254;
-        weightModifier = 0.453592;
-        
+        String personsHeightInches;
+        String personsWeightPounds;
+        int personsHeight;
+        int personsWeight;
+        double personsHeightConverted;
+        double personsWeightConverted;
+        double personsBMI;
+
+        Scanner keyboard = new Scanner(System.in);
+
         System.out.println("Hello this is my BMI Calculator! Lets start off with your name.");
         personsName = keyboard.nextLine();
         System.out.println("Hi " + personsName + "! Next what is your height in inches?");
-        personsHeight = keyboard.nextLine();
+        personsHeightInches = keyboard.nextLine();
         System.out.println("My last question, if you don't mind me asking what's your weight in pounds?");
-        personsWeight = keyboard.nextLine();
-        
-        convertedHeight = personsHeight * heightModifier;
-        convertedWeight = personsWeight * weightModifier;
-        personsBMI = (convertedHeight/convertedWeight)/convertedWeight;
-        
-        System.out.println("Thank you for the information! I calculated your BMI to be " + personsBMI + ".");
+        personsWeightPounds = keyboard.nextLine();
 
+        personsHeight = Integer.parseInt(personsHeightInches);
+        personsHeightConverted = (personsHeight*0.0254);
+        personsWeight = Integer.parseInt(personsWeightPounds);
+        personsWeightConverted = (personsWeight*0.453592);
+        personsBMI = (personsWeightConverted/personsHeightConverted)/personsHeightConverted;
 
-
-
+        System.out.println("Thanks for the information! You're BMI is " + personsBMI);
     }
 }
