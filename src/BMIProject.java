bf56aa4cd40c658bc5ecfa07b1418b1bd83fd068
Unknown <brandon.woolard.2019@student.lhschools.org>
2018-09-06 16:53:25 -0400

    Added feet to inches converter

--- src/BMIProject.java
+++ src/BMIProject.java
@@ -1,8 +1,47 @@
 import java.util.Scanner;
 
 public class BMIProject {
-
     public static void main(String[] args) {
+        String personsName;
+        String personsHeightInches;
+        String personsHeightFeet;
+        String personsWeightPounds;
+        int personsHeight;
+        int personsWeight;
+        int personsHeightFeetCombine;
+        int personsHeightInchesCombine;
+        int personsHeightCombined;
+        double personsHeightConverted;
+        double personsWeightConverted;
+        double personsBMI;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Hello this is my BMI Calculator! Lets start off with your name.");
+        personsName = keyboard.nextLine();
+        System.out.println("Hi " + personsName + "! Next what is your height in feet?");
+        personsHeightFeet = keyboard.nextLine();
+        System.out.println("Inches?");
+        personsHeightInches = keyboard.nextLine();
+        System.out.println("My last question, if you don't mind me asking what's your weight in pounds?");
+        personsWeightPounds = keyboard.nextLine();
+
+        personsHeightFeetCombine = Integer.parseInt(personsHeightFeet);
+        personsHeightInchesCombine = Integer.parseInt(personsHeightInches);
+        personsHeightCombined = personsHeightInchesCombine + (personsHeightFeetCombine * 12);
+        personsHeight = Integer.parseInt(String.valueOf(personsHeightCombined));
+        personsHeightConverted = (personsHeight*0.0254);
+        personsWeight = Integer.parseInt(personsWeightPounds);
+        personsWeightConverted = (personsWeight*0.453592);
+        personsBMI = (personsWeightConverted/personsHeightConverted)/personsHeightConverted;
+
+        System.out.println("Thanks for the information" + personsName + "! Your BMI is " + personsBMI + ". Feel free " +
+                "to use this calculator again, have a nice day!");
+    }
+
+
+    /*BMI Calculator Inches Already Converted
+      public static void main(String[] args) {
         String personsName;
         String personsHeightInches;
         String personsWeightPounds;
@@ -27,7 +66,7 @@ public class BMIProject {
         personsWeightConverted = (personsWeight*0.453592);
         personsBMI = (personsWeightConverted/personsHeightConverted)/personsHeightConverted;
 
-        System.out.println("Thanks for the information! Your BMI is " + personsBMI + ". Feel free to use this calculator again. Have" + 
-        "a nice day!");
-    }
+        System.out.println("Thanks for the information" + personsName + "! Your BMI is " + personsBMI + ". Feel free " +
+                           "to use this calculator again, have a nice day!");
+    }*/
 }
